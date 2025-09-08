3f08ac427df0ac1dce7d6a7c4efa410a957574a2
sukanta022 <sukantacb20@gmail.com>
2025-09-08 13:20:07 +0600

    added cashout

--- script/addMoney.js
+++ script/addMoney.js
@@ -29,3 +29,51 @@ document.getElementById("add-money-btn").addEventListener("click", function(e){
         document.getElementById("account-num").value = "";
     }
 });
+
+//withdraw features
+document.getElementById("withdraw-btn").addEventListener("click", function(e){
+    e.preventDefault();
+    defaultAgenttNumber = 12345678910;
+    defaultDigit = 1234;
+
+    inputNumber = parseInt(document.getElementById("agent-number").value);
+    withdrawAmount = Number(document.getElementById("withdraw-amount").value);
+    accountPin = parseInt(document.getElementById("w-pin").value);
+
+
+    if(inputNumber===defaultAgenttNumber && accountPin===defaultDigit){
+        availableBalance = Number(document.getElementById("total-amount").innerText);
+        if(availableBalance>=withdrawAmount){
+            availableBalance = availableBalance - withdrawAmount;
+            document.getElementById("total-amount").innerText = availableBalance;
+            alert("Withdraw Complete");
+
+            document.getElementById("agent-number").value = "";
+            document.getElementById("withdraw-amount").value = "";
+            document.getElementById("w-pin").value = "";
+        }
+        else{
+            alert("insufficient balance");
+            document.getElementById("withdraw-amount").value = "";
+        }
+    }
+    else{
+        alert("invalid credential");
+        document.getElementById("agent-number").value = "";
+        document.getElementById("w-pin").value = "";
+    }
+
+    
+
+});
+
+//toggle features
+document.getElementById("add-money").addEventListener("click", function(){
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("add-money-parent").style.display = "block";
+});
+
+document.getElementById("cash-out").addEventListener("click", function(){
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "block";
+});
